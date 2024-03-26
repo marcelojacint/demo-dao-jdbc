4f5d4f2ab50f7f1d53c04ffb9477aee7a53ded80
Marcelo J Ferreira <marcelojacintoferreirq@gmail.com>
2024-03-26 20:31:31 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "cap");
-		System.out.println(obj);
+
+		Seller seller = new Seller(1, "jose", "jose@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 
 	}
 
